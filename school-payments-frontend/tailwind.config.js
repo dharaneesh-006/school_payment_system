4ef8c77cd8ee09676dc122fa8433bb1e01e7620d
dharaneesh-006 <dharaneesh112@gmail.com>
2025-09-23 13:40:06 +0530

    index html

---
+++ school-payments-frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: [
+    "./index.html",
+    "./src//*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // <-- add this line
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
